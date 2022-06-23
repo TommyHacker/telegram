95db8900b943435fcb8719d8603d6b9b6ce49bfb
TommyHacker <t.hacker.webdev@gmail.com>
2022-06-23 12:55:48 +0100

    front end fetch and render done, with styles

--- client/public/scripts/getAll.js
+++ client/public/scripts/getAll.js
@@ -1,5 +1,29 @@
+const container = document.querySelector(".telegraphs-container");
 const getAllTelegraphs = async () => {
-    const results = await fetch("http://localhost:3000/telegraphs");
-    const data = await results.json()
-    console.log(data)
-}
+  const results = await fetch("http://localhost:3000/telegraphs");
+  const { data } = await results.json();
+
+  data.map((d) => {
+    const card = document.createElement("div");
+    card.className = "card";
+    const title = document.createElement("h1");
+    const name = document.createElement("p");
+    const body = document.createElement("p");
+
+    title.textContent = d.title;
+    name.textContent = d.pseudonym;
+    body.textContent = d.body;
+
+    title.className = "card-title";
+    name.className = "card-name";
+    body.className = "card-body";
+
+    card.append(title);
+    card.append(name);
+    card.append(body);
+
+    container.append(card);
+  });
+};
+
+getAllTelegraphs();
